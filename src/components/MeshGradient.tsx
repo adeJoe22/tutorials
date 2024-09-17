cb4697e32c0dca66197438631f8af2c3dc9378a0
Joseph Omogbehin <adedejiomogbehin.com>
2024-09-17 18:33:53 +0100

    Hero

---
+++ src/components/MeshGradient.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const MeshGradientBackground: React.FC = () => {
+  return (
+    <div className='mesh-gradient-background'>
+      <div className='mesh-layer'></div>
+      <div className='color-element color-one'></div>
+      <div className='color-element color-two'></div>
+      <div className='color-element color-three'></div>
+    </div>
+  );
+};
+
+export default MeshGradientBackground;
---
+++ src/pages/Hero.tsx
@@ -0,0 +1,28 @@
+import MeshGradientBackground from "../components/MeshGradient";
+
+function Hero() {
+  return (
+    <div className='relative h-screen flex items-center justify-center'>
+      {/* Animated Mesh Gradient Background */}
+      <MeshGradientBackground />
+
+      {/* Caption Content */}
+      <div className='absolute z-10 text-center text-black max-w-3xl px-6 md:px-8 lg:px-12'>
+        <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-tight'>
+          Ready to switch careers and break into tech?
+        </h1>
+        <p className='mt-4 text-base sm:text-lg md:text-xl lg:text-2xl font-light'>
+          Join our hands-on Full Stack Software Development training
+          program designed specifically for career changers. Gain the
+          skills, confidence, and support you need to thrive in the
+          digital world!
+        </p>
+        <button className='mt-8 px-5 py-3 sm:px-6 sm:py-3 md:px-8 md:py-4 bg-[#B1B2FF] text-white font-medium rounded-lg shadow-lg hover:bg-indigo-700 transition duration-300 ease-in-out'>
+          Get Started Today
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
